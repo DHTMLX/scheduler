ebfb941ac90f7b8b75262171f4866f47df7019b1
Denys Krasnoshchok <Denys Krasnoshchok>
2015-12-23 11:22:17 +0100

    Bugfix for invalid date/time calculations for some DST time zones

--- codebase/sources/dhtmlxscheduler.js
+++ codebase/sources/dhtmlxscheduler.js
@@ -3333,10 +3333,20 @@ scheduler.date={
 		var ndate = new Date(date.valueOf());
 
 		ndate.setDate(ndate.getDate() + inc);
+
+		if(inc == Math.round(inc) && inc > 0){
+			var datesDiff = +ndate - +date,
+				rest = datesDiff % (24*60*60*1000);
+			if(rest != 0 && date.getTimezoneOffset() == ndate.getTimezoneOffset()){
+				var hours = rest / (60* 60 * 1000);
+				ndate.setTime(ndate.getTime() + (24 - hours) * 60 * 60 * 1000);
+			}
+		}
+
 		if (inc >= 0 && (!date.getHours() && ndate.getHours()) &&//shift to yesterday on dst
 			(ndate.getDate() < date.getDate() || ndate.getMonth() < date.getMonth() || ndate.getFullYear() < date.getFullYear()) )
 			ndate.setTime(ndate.getTime() + 60 * 60 * 1000 * (24 - ndate.getHours()));
-		   return ndate;
+		return ndate;
 	},
 	add:function(date,inc,mode){
 		var ndate=new Date(date.valueOf());
